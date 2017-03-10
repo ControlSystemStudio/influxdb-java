eba340b1bba5441c1299a3ac7771125e5885804e
Megan Grodowitz <grodowitzml@ornl.gov>
2017-03-10 13:40:50 -0500

    Fix docker start script

--- docker-start.sh
+++ docker-start.sh
@@ -1,3 +1,5 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 docker kill influxdb || true
 docker rm influxdb || true
 docker run \
@@ -5,5 +7,5 @@ docker run \
        --name influxdb \
        --publish 8086:8086 \
        --publish 8089:8089/udp \
-       --volume ${PWD}/influxdb.conf:/etc/influxdb/influxdb.conf \
+       --volume ${DIR}/influxdb.conf:/etc/influxdb/influxdb.conf \
        influxdb:1.2-alpine
